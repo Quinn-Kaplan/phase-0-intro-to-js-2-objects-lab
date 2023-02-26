f727ded2e827e802d3c38ea06106eea952f5da18
Quinn Kaplan <Quinnkaplan@gmail.com>
2023-02-26 13:55:25 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: "inster real value here",
+    streetAdress: "add real value later",
+};
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    return {... obj,[key] : value,};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj,key){
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+    return obj;
+}
